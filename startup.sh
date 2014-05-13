bf693efdcd4e74fbc2bb1a9fba0719a6fcefc68c
danielw <daniel.wolf@ravellosystems.com>
2014-05-13 15:00:09 +0300

    The startup script should start the 2 servers of the application.

--- startup.sh
+++ startup.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 cd $RAVELLO_TRAINING_HOME
-
 cd ./training-server
-NODE_ENV="production" node server.js
+NODE_ENV="production" node server.js &
+
+cd $RAVELLO_TRAINING_HOME
+cd ./training-webapp
+grunt serve &
 
