d5f8602b4bf252e6d0663742f39aef301c29da60
danielw <daniel.wolf@ravellosystems.com>
2014-05-13 13:52:28 +0300

    Making a clear init_db sciprt.

---
+++ init_db.js
@@ -0,0 +1,16 @@
+
+var adminUser = {
+    _id: ObjectId('aaaaaaaaaaaaaaaaaaaaaaaa'),
+    firstName : "Admin",
+    surname : "",
+    username : "admin",
+    password : "$2a$10$hQR3dSKHMdANwm1uU3T.1uX6b5HoTZFLbN1YhsM2RggIYaToCOj02",
+    role : "ADMIN"
+};
+
+db.trainingclasses.remove();
+db.trainingcourses.remove();
+db.users.remove();
+
+db.users.save(adminUser);
+
