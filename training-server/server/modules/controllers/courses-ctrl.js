0e90a32a91478cdc57ec10ed5044ed0a921ba425
danielw <daniel.wolf@ravellosystems.com>
2014-05-22 10:53:40 +0300

    Bug fix - when saving a course, the returned JSON didn't hold all of the data of the course (data that IS returned on the GET call), resulting in missing data in the GUI. The logic for returning a Course resource should be the same in either GET, PUT or POST.

--- training-server/server/modules/controllers/courses-ctrl.js
+++ training-server/server/modules/controllers/courses-ctrl.js
@@ -12,6 +12,8 @@ var coursesService = require('../services/courses-service');
 
 var coursesTrans = require('../trans/courses-trans');
 
+/* --- Public functions --- */
+
 exports.getCourses = function(request, response) {
     var user = request.user;
 
@@ -24,12 +26,11 @@ exports.getCourses = function(request, response) {
 
             return q.all(_.map(courses, function(courseEntity) {
                 var course = coursesTrans.entityToDto(courseEntity);
-                return q.all(_.map(course.blueprints, function(bp) {
-                    return blueprintsService.getBlueprintById(bp.id, ravelloUsername, ravelloPassword);
-                })).then(
-                    function(bpResults) {
-                        course.blueprints = coursesService.assignBlueprintsToCourse(course, bpResults);
-                        courseDtos.push(course);
+
+                return matchCourseWithBlueprints(courseEntity, ravelloUsername, ravelloPassword).then(
+                    function(result) {
+                        courseDtos.push(result);
+                        return result;
                     }
                 ).fail(
                     function(error) {
@@ -61,22 +62,20 @@ exports.getCourse = function(request, response) {
     var ravelloUsername = user.ravelloCredentials.username;
     var ravelloPassword = user.ravelloCredentials.password;
 
-    coursesDal.getCourse(courseId).then(function(courseEntity) {
-        var courseDto = coursesTrans.entityToDto(courseEntity);
-        return q.all(_.map(courseDto.blueprints, function(bp) {
-            return blueprintsService.getBlueprintById(bp.id, ravelloUsername, ravelloPassword);
-        })).then(
-            function(bpResults) {
-                courseDto.blueprints = coursesService.assignBlueprintsToCourse(courseDto, bpResults);
-                response.json(courseDto);
-            }
-        ).fail(
-            function(error) {
-                var message = "Could not get one of the course's blueprints";
-                logger.error(error, message);
-                response.send(404, message);
-            }
-        );
+    coursesDal.getCourse(courseId).then(
+        function(courseEntity) {
+            var courseDto = coursesTrans.entityToDto(courseEntity);
+            return matchCourseWithBlueprints(courseDto, ravelloUsername, ravelloPassword).then(
+                function(result) {
+                    response.json(result);
+                }
+            ).fail(
+                function(error) {
+                    var message = "Could not get one of the course's blueprints";
+                    logger.error(error, message);
+                    response.send(404, message);
+                }
+            );
         }
     ).fail(
         function(error) {
@@ -88,10 +87,25 @@ exports.getCourse = function(request, response) {
 };
 
 exports.createCourse = function(request, response) {
+    var user = request.user;
+
+    var ravelloUsername = user.ravelloCredentials.username;
+    var ravelloPassword = user.ravelloCredentials.password;
+
     coursesDal.createCourse(request.body).then(
         function(courseEntity) {
             var course = coursesTrans.entityToDto(courseEntity);
-            response.json(courseEntity);
+            return matchCourseWithBlueprints(course, ravelloUsername, ravelloPassword).then(
+                function(result) {
+                    response.json(result);
+                }
+            ).fail(
+                function(error) {
+                    var message = "Could not get one of the course's blueprints";
+                    logger.error(error, message);
+                    response.send(404, message);
+                }
+            );
         }
     ).fail(
         function(error) {
@@ -103,6 +117,11 @@ exports.createCourse = function(request, response) {
 };
 
 exports.updateCourse = function(request, response) {
+    var user = request.user;
+
+    var ravelloUsername = user.ravelloCredentials.username;
+    var ravelloPassword = user.ravelloCredentials.password;
+
     var courseId = request.params.courseId;
     var courseData = request.body;
 
@@ -111,7 +130,17 @@ exports.updateCourse = function(request, response) {
             return coursesDal.getCourse(courseId).then(
                 function(result) {
                     var dto = coursesTrans.entityToDto(result);
-                    response.json(dto);
+                    return matchCourseWithBlueprints(dto, ravelloUsername, ravelloPassword).then(
+                        function(result) {
+                            response.json(result);
+                        }
+                    ).fail(
+                        function(error) {
+                            var message = "Could not get one of the course's blueprints";
+                            logger.error(error, message);
+                            response.send(404, message);
+                        }
+                    );
                 }
             );
         }
@@ -136,6 +165,19 @@ exports.deleteCourse = function(request, response) {
             var message = "Could not delete course";
             logger.error(error, message);
             response.send(404, message);
+        }
+    );
+};
+
+/* --- Private functions --- */
+
+var matchCourseWithBlueprints = function(course, ravelloUsername, ravelloPassword) {
+    return q.all(_.map(course.blueprints, function(bp) {
+        return blueprintsService.getBlueprintById(bp.id, ravelloUsername, ravelloPassword);
+    })).then(
+        function(bpResults) {
+            course.blueprints = coursesService.assignBlueprintsToCourse(course, bpResults);
+            return course;
         }
     );
 };
