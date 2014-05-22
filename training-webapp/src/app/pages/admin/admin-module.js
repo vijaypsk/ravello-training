f8d189d545cda2690a441f65eb26e0a864b942d2
danielw <daniel.wolf@ravellosystems.com>
2014-05-22 09:50:02 +0300

    Conventions refactoring.

--- training-webapp/src/app/pages/admin/admin-module.js
+++ training-webapp/src/app/pages/admin/admin-module.js
@@ -2,38 +2,39 @@
 
 (function (angular) {
     angular.module('trng.admin', [
-            'trng.config',
-            'trng.admin.profile',
-            'trng.admin.trainers',
-            'trng.services',
-            'trng.login',
-            'ui.router'
-        ]).
-        config([
-            '$urlRouterProvider',
-            '$stateProvider',
-            'StatesNames',
-            function($urlRouterProvider, $stateProvider, StatesNames) {
+        'trng.config',
+        'trng.admin.profile',
+        'trng.admin.trainers',
+        'trng.services',
+        'trng.login',
+        'ui.router'
+    ]);
 
-                $urlRouterProvider.when("/admin", "/admin/trainers");
+    angular.module('trng.admin').config([
+        '$urlRouterProvider',
+        '$stateProvider',
+        'StatesNames',
+        function($urlRouterProvider, $stateProvider, StatesNames) {
 
-                $stateProvider.
-                    state(StatesNames.admin.profile.name, {
-                        url: '/profile',
-                        templateUrl: 'app/pages/admin/profile/admin-profile.html',
-                        controller: 'adminProfileController'
-                    }).state(StatesNames.admin.trainers.name, {
-                        url: '/trainers',
-                        templateUrl: 'app/pages/admin/trainers/admin-trainers.html',
-                        controller: 'adminTrainersController',
-                        resolve: adminTrainerResolver
-                    }).state(StatesNames.admin.singleTrainer.name, {
-                        url: '/single-trainer?trainerId',
-                        templateUrl: 'app/pages/admin/trainers/single-trainer/admin-single-trainer.html',
-                        controller: 'adminSingleTrainerController',
-                        resolve: adminSingleTrainerResolver
-                    });
-            }
-        ]);
+            $urlRouterProvider.when("/admin", "/admin/trainers");
+
+            $stateProvider.
+                state(StatesNames.admin.profile.name, {
+                    url: '/profile',
+                    templateUrl: 'app/pages/admin/profile/admin-profile.html',
+                    controller: 'adminProfileController'
+                }).state(StatesNames.admin.trainers.name, {
+                    url: '/trainers',
+                    templateUrl: 'app/pages/admin/trainers/admin-trainers.html',
+                    controller: 'adminTrainersController',
+                    resolve: adminTrainerResolver
+                }).state(StatesNames.admin.singleTrainer.name, {
+                    url: '/single-trainer?trainerId',
+                    templateUrl: 'app/pages/admin/trainers/single-trainer/admin-single-trainer.html',
+                    controller: 'adminSingleTrainerController',
+                    resolve: adminSingleTrainerResolver
+                });
+        }
+    ]);
 })(angular);
 
