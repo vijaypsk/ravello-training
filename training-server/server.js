6c93bc274b7a9b5d19f24bc2f5015da7731534a7
danielw <daniel.wolf@ravellosystems.com>
2015-04-30 16:30:00 +0300

    Changing the way the server is initialized and run, so that it can run in single mode and cluster mode.

--- training-server/server.js
+++ training-server/server.js
@@ -10,17 +10,34 @@ var cluster = require('express-cluster');
 var properties = require('./server/modules/config/properties');
 var logger = require('./server/modules/config/logger');
 
-cluster(function(worker) {
-	// console.log('Starting worker #' + worker.id);
-	logger.info('Starting worker # %s', worker.id);
+var config = require('./server/modules/config/config');
+var routes = require('./server/modules/config/routes');
+var errorHandler = require('./server/modules/utils/error-handler');
 
-	var config = require('./server/modules/config/config');
-	var routes = require('./server/modules/config/routes');
-
+function run() {
 	var app = express();
 
 	config(app);
 	routes(app);
 
 	app.listen(app.get('port'));
-}, {count: properties.numOfWorkers});
+}
+
+function runSingle() {
+	logger.info('Starting in single mode, no cluster');
+	run();
+}
+
+function runCluster() {
+	cluster(function(worker) {
+		logger.info('Starting worker # %s', worker.id);
+		run();
+	}, {count: properties.numOfWorkers});
+}
+
+if (properties.numOfWorkers === 1) {
+	runSingle();
+} else {
+	runCluster();
+}
+
