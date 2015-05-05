0212442605594f06a245fbca1f85669dcaa02a89
danielw <daniel.wolf@ravellosystems.com>
2015-05-05 19:04:32 +0300

    Adding a script that sets the hostname for the remote_syslog system, in order to support remote logging for the TP.

---
+++ bin/configure_remote_syslog_hostname.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+hostname=$(cat /etc/ravello/vm.json | sed -e 's/,/\n/g' | grep fqdns | awk -F: '{print $2}' | sed -e 's/["}\[]//g' | sed -e 's/\]//g')
+
+cat /etc/log_files.yml | grep -v hostname > /tmp/log_files.yml
+echo "hostname: $hostname" >> /tmp/log_files.yml
+
+cp /tmp/log_files.yml /etc/log_files.yml
+
