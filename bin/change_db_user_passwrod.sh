a6620598b074ab9f691eb3a5fbfb091fea9c7df9
danielw <daniel.wolf@ravellosystems.com>
2015-05-07 16:38:46 +0300

    Adding usage.

--- bin/change_db_user_passwrod.sh
+++ bin/change_db_user_passwrod.sh
@@ -9,5 +9,5 @@ username=$1
 existingPassword=$2
 newPassword=$3
 
-mongo -u $username -p $existingPassword training --eval "db.removeUser('training'); db.addUser('training', '$newPassword');"
+mongo -u $username -p $existingPassword training --eval "db.removeUser('$username'); db.addUser('$username', '$newPassword');"
 
