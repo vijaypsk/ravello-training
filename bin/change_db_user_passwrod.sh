d46d77d7f1e5ba5ca6bde9e97b98fbe90b09f2dd
danielw <daniel.wolf@ravellosystems.com>
2015-05-07 14:06:42 +0300

    Adding usage to one of the scripts.

--- bin/change_db_user_passwrod.sh
+++ bin/change_db_user_passwrod.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$#" -ne 3 ]; then
+    echo "Usage: change_db_user_passwrod.sh <username> <existing password> <new password>"
+    exit 1
+fi
+
 username=$1
 existingPassword=$2
 newPassword=$3
