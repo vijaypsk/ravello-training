3f56e4cdbfa1b42863da8138c4c6dc7c90e48d99
danielw <daniel.wolf@ravellosystems.com>
2015-05-19 16:45:39 +0300

    Add validation on TP password when creating a new Trainer user.

--- training-webapp/src/app/pages/admin/trainers/single-trainer/admin-single-trainer-ctrl.js
+++ training-webapp/src/app/pages/admin/trainers/single-trainer/admin-single-trainer-ctrl.js
@@ -5,17 +5,32 @@ angular.module('trng.admin.trainers').controller('adminSingleTrainerController',
     '$scope',
     '$window',
     '$state',
+    '$dialogs',
     'StatesNames',
     'TrainersService',
     'currentTrainer',
-    function($log, $scope, $window, $state, StatesNames, TrainersService, currentTrainer) {
+    function($log, $scope, $window, $state, $dialogs, StatesNames, TrainersService, currentTrainer) {
         $scope.init = function() {
             $scope.currentTrainer = currentTrainer;
         };
 
         $scope.saveTrainer = function() {
+            var validationStatus = getValidationStatus();
+
+            if (!_.isEmpty(validationStatus)) {
+                var finalMessage = _.reduce(validationStatus, function(sum, current) {
+                    return sum += ", " + current;
+                });
+
+                finalMessage = "Could not save, please fill required fields: [" + finalMessage + "]";
+
+                $dialogs.error('Validation error', finalMessage);
+
+                return;
+            }
+
             return TrainersService.saveOrUpdate($scope.currentTrainer).then(
-                function(result) {
+                function() {
                     $state.go(StatesNames.admin.trainers.name);
                 }
             );
@@ -28,6 +43,17 @@ angular.module('trng.admin.trainers').controller('adminSingleTrainerController',
         $scope.isSaveDisabled = function() {
             return $scope.singleTrainerForm ? !$scope.singleTrainerForm.$valid : false;
         };
+
+        function getValidationStatus() {
+            var validationStatus = [];
+
+            !$scope.currentTrainer.firstName && validationStatus.push("First name");
+            !$scope.currentTrainer.username && validationStatus.push("Training Portal Username");
+
+            !$scope.currentTrainer.id && !$scope.currentTrainer.password && validationStatus.push("password");
+
+            return validationStatus;
+        }
 
         $scope.init();
     }
