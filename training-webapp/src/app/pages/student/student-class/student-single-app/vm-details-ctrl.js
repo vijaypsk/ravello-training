5b3735538db80249568427acec070d40713f1ac4
danielw <daniel.wolf@ravellosystems.com>
2015-05-25 17:35:41 +0300

    Handling the case in which a single service has many ports. We want to have a 'Go!' link for each such port, so we need to show each in a row of its own in the 'Go!' dialog.

--- training-webapp/src/app/pages/student/student-class/student-single-app/vm-details-ctrl.js
+++ training-webapp/src/app/pages/student/student-class/student-single-app/vm-details-ctrl.js
@@ -9,6 +9,10 @@ angular.module('trng.student').controller('studentVmDetailsController', [
         $scope.init = function() {
             $scope.selectedVm = selectedVm;
             $scope.dnsWithExternalServices = $scope.determineDnsWithExternalServices();
+
+            _.forEach(selectedVm.allDns, function(dns) {
+                initServicesViewModel(dns);
+            });
         };
 
         $scope.determineDnsWithExternalServices = function() {
@@ -46,7 +50,25 @@ angular.module('trng.student').controller('studentVmDetailsController', [
 			return isHttp(service) || isHttps(service);
 		};
 
-		function isHttp(service) {
+        function initServicesViewModel(dns) {
+            dns.servicesViewModel = [];
+
+            _.forEach(dns.services, function(service) {
+                var ports = service.port.split(',');
+                var externalPorts = service.externalPort.split(',');
+
+                _.forEach(ports, function(port, index) {
+                    dns.servicesViewModel.push({
+                        name: service.name,
+                        port: port,
+                        protocol: service.protocol,
+                        externalPort: externalPorts[index]
+                    });
+                });
+            });
+        }
+
+        function isHttp(service) {
 			return (service.protocol ? service.protocol === 'HTTP' : false) ||
 				(service.externalPort ? service.port === 80 || service.port === 8080 : false) ||
 				(service.name ? service.name === 'http' || service.name === 'www' || service.name === 'web' : false);
